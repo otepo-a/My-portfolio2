e450bc873c5bd255c2ce80ec8486ddd0f4e37d6a
otepo <malundas23@gmail.com>
2026-02-26 08:29:40 +0800

    first commit

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburger-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
 
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.nav1');
-  const scrolled = window.scrollY > navbar.offsetHeight;
-  navbar.classList.toggle('navbar-scrolled', scrolled);
+window.addEventListener('scroll', function () {
+  const desktopNav = document.querySelector('.nav1');
+  const hamburgerNav = document.querySelector('#hamburger-nav');
+  const scrolled =
+    window.scrollY >
+    (desktopNav
+      ? desktopNav.offsetHeight
+      : hamburgerNav
+        ? hamburgerNav.offsetHeight
+        : 0);
+  if (desktopNav) desktopNav.classList.toggle('navbar-scrolled', scrolled);
+  if (hamburgerNav) hamburgerNav.classList.toggle('navbar-scrolled', scrolled);
 });
